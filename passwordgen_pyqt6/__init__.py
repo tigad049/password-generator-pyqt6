ba80a68f4380282bda20517d93bef44f6f0a49a3
tigad049 <tigad049@hsd.k12.or.us>
2023-04-07 13:30:26 -0700

    Organize code, add comments, add new way of adding widgets to layouts

--- passwordgen_pyqt6/__init__.py
+++ passwordgen_pyqt6/__init__.py
@@ -1,7 +1,8 @@
 __version__ = '0.1.0'
 
 import sys
-from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtCore import QSize, Qt, QObject
+from PyQt6.QtGui import QRegularExpressionValidator
 from PyQt6.QtWidgets import (
     QApplication, 
     QMainWindow,
@@ -10,7 +11,9 @@ from PyQt6.QtWidgets import (
     QCheckBox,
     QLineEdit,
     QLabel,
-    QWidget
+    QWidget,
+    QSlider,
+    QSpinBox
 )
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,25 +22,46 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Password Generator")
         self.setFixedSize(QSize(400, 600))
         
-        uppercase_checkbox = QCheckBox("Uppercase Letters")
-        lowercase_checkbox = QCheckBox("Lowercase Letters")
-        numbers_checkbox = QCheckBox("Include Numbers")
-        symbols_checkbox = QCheckBox("Include Symbols")
-        symbols_lineedit = QLineEdit("@%+\\/'!#$^?:,(){}[]~`-_.")
+        widgets = {}
+        layouts = {}
         
-        password_options_layout = QVBoxLayout()
-        password_options_layout.addWidget(uppercase_checkbox)
-        password_options_layout.addWidget(lowercase_checkbox)
-        password_options_layout.addWidget(numbers_checkbox)
-        password_options_layout.addWidget(symbols_checkbox)
-        password_options_layout.addWidget(symbols_lineedit)
+        # "PASSWORD OPTIONS" WIDGETS
+        # Create checkboxes
+        widgets[0] = uppercase_checkbox = QCheckBox("Uppercase Letters")
+        widgets[1] = lowercase_checkbox = QCheckBox("Lowercase Letters")
+        widgets[2] = numbers_checkbox = QCheckBox("Include Numbers")
+        widgets[3] = symbols_checkbox = QCheckBox("Include Symbols")
         
+        # Check all checkboxes
+        for i in widgets:
+            widgets[i].setCheckState(Qt.CheckState.Checked)
+        
+        # Create symbol edit box
+        widgets[4] = symbols_lineedit = QLineEdit("@%+\\/'!#$^?:,(){}[]~`-_.")
+        
+        # Create password length controls
+        widgets[5] = length_slider = QSlider(Qt.Orientation.Horizontal)
+        widgets[6] = length_spinbox = QSpinBox()
+        length_slider.setRange(3, 100)
+        length_spinbox.setRange(3, 100)
+        
+        # Add password options widgets to layout
+        layouts[0] = password_options_layout = QVBoxLayout()
+        for widget in widgets:
+            password_options_layout.addWidget(widgets[widget])
+        widgets.clear()
+        
+        # Create main GUI layout
         main_layout = QVBoxLayout()
-        main_layout.addLayout(password_options_layout)
         
-        main_widget = QWidget()
-        main_widget.setLayout(main_layout)
-        self.setCentralWidget(main_widget)
+        # Add all currently defined layouts
+        for i in layouts:
+            main_layout.addLayout(layouts[i])
+        
+        # Display main GUI layout
+        main_screen = QWidget()
+        main_screen.setLayout(main_layout)
+        self.setCentralWidget(main_screen)
 
 app = QApplication(sys.argv)
 
